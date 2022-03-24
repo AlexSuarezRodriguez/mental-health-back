0515bef440e98068d0056f6e45a2f6eaf996808a
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2022-03-24 11:33:21 -0500

    feat: punto de entrada a nuestra app

---
+++ app.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const configExpress = require('./config/express');
+const routes = require('./routes');
+
+const app = express();
+
+configExpress(app);
+routes(app);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
