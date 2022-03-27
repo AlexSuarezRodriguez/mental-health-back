b5270a0a5b2b68937d6757b7332776ecc829bf2c
Diego Cubides <95053070+d1360cub@users.noreply.github.com>
2022-03-27 15:12:59 -0500

    adds users api (#2)
    Co-authored-by: ojosdegatonegro <diegofernandocubides@gmail.com>

---
+++ api/user/index.js
@@ -0,0 +1,18 @@
+const { Router } = require('express');
+const {
+  handlerAllUsers,
+  handlerGetOneUser,
+  handlerCreateUser,
+  handlerUpdateUser,
+  handlerDeleteUser,
+} = require('./user.controller');
+const { createUser } = require('./user.service');
+
+const router = Router();
+router.get('/', handlerAllUsers)
+router.get('/:id', handlerGetOneUser)
+router.post('/', handlerCreateUser)
+router.patch('/:id', handlerUpdateUser)
+router.delete('/:id', handlerDeleteUser)
+
+modules.exports = router;
