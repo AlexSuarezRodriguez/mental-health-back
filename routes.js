cac97a80408700d2b0650120962176d65311937b
popehiflo <poolpetterhijuel@hotmail.com>
2022-03-28 03:11:03 -0500

    feat: main route specialities

--- routes.js
+++ routes.js
@@ -1,9 +1,9 @@
-// const specialty = require('./api/speciality');
+const specialty = require('./api/speciality');
 const user = require('./api/user');
 const service = require('./api/service');
 
 function routes(app) {
-  // app.use('/api/specialities', specialty);
+  app.use('/api/specialities', specialty);
   app.use('/api/users', user);
   app.use('/api/services', service);
 }
