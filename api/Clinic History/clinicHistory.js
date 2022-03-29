e9be29df85da21b9a69976fae219b6530a28e225
wendy-medina <endyleyms@gmail.com>
2022-03-29 17:11:17 -0500

    Creación modelos Historia clínica y citas

---
+++ api/Clinic History/clinicHistory.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose');
+const clinicHistory = new mongoose.Schema({
+    idpatient:{type: Number, required: true},
+    iddoctor:{type: Number, required: true},
+    description: {type: String, required: true},
+    createdate: {type: Number, required: true},
+    updatedate:{type: Number, required: true}
+},
+{
+    timestamps: true,
+    versionKey: false,
+  })
+
+module.exports = mongoose.model('clinicHistory', clinicHistory);
