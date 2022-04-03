d529eb290005b24153b1a50377ad84d0dd2ec70e
wendy-medina <endyleyms@gmail.com>
2022-04-02 23:31:29 -0500

    Validación Historia clinica

--- api/clinicHistory/clinicHistory.model.js
+++ api/clinicHistory/clinicHistory.model.js
@@ -3,7 +3,13 @@ const clinicHistory = new mongoose.Schema({
     //idpatient:{type: Number, required: true},
     //iddoctor:{type: Number, required: true},
     description: {type: String, required: true},
+    userId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'User',
+      required: true,
+  },
 },
+  
 {
     timestamps: true,
     versionKey: false,
--- api/clinicHistory/index.js
+++ api/clinicHistory/index.js
@@ -1,4 +1,5 @@
 const {Router} = require ('express');
+const {isAuthenticated, hasRole}=require('../../auth/auth.service')
 const {
     handlerAllClinicHistory,
     handlerOneClinicHistory,
@@ -8,10 +9,10 @@ const {
  } = require ('./clinicHistory.controller');
 const router = Router();
 
-router.get('/', handlerAllClinicHistory)
-router.get('/:id', handlerOneClinicHistory)
-router.post('/', handlerCreateChistory)
-router.put('/:id', handlerUpdateChistory)
-router.delete('/:id', handlerDeleteChistory)
+router.get('/', isAuthenticated(), handlerAllClinicHistory)
+router.get('/:id', isAuthenticated(), handlerOneClinicHistory)
+router.post('/', isAuthenticated(), handlerCreateChistory)
+router.patch('/:id', hasRole(['doctor']), handlerUpdateChistory)
+router.delete('/:id', hasRole(['doctor']), handlerDeleteChistory)
 
 module.exports = router
