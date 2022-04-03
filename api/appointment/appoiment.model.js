da5e2760240fb1d83012069c0c16a67183395b2f
wendy-medina <endyleyms@gmail.com>
2022-04-03 01:09:47 -0500

    creacion colección Appoiments

--- api/appointment/appoiment.model.js
+++ api/appointment/appoiment.model.js
@@ -1,14 +1,19 @@
 const mongoose = require ('mongoose');
-const appoimentSchema = new mongoose.Schema({
+const appoiment = new mongoose.Schema({
     stardate:{type: Number, required: true},
     enddate: {type: Number, default: true},
-    idpatient:{type: Number, required: true},
-    iddoctor:{type: Number, required: true},
-    price:{type: Number, required: true}
+    //idpatient:{type: Number, required: true},
+    //iddoctor:{type: Number, required: true},
+    price:{type: Number, required: true},
+    userId:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:'User',
+      required: true,
+  },
 },
 {
     timestamps: true,
     versionKey: false,
   })
 
-module.exports = mongoose.model('appoiment', appoimentSchema);
+module.exports = mongoose.model('appoiment', appoiment);
