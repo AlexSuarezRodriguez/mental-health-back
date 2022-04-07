91d3b1d8db6e4b44ce9cbc8fcdfbf2757cbe469a
Alexander suarez rodriguez <87741454+AlexSuarezRodriguez@users.noreply.github.com>
2022-04-06 21:33:48 -0500

    refactorizacion en user.test.js (#12)
    * refactorizacion en user.test.js
    
    * no comentarios
    
    *se resuelven conflictos

--- api/user/user.test.js
+++ api/user/user.test.js
@@ -1,77 +1,29 @@
 const supertest= require('supertest');
+const mongoose = require('mongoose');
 
-const app = require('../../app');
+const app = require('../../app')
 const connectDB = require('../../config/database');
+const users = require('./user.model');
 
 const request = supertest(app);
 
 describe('users EndPoints', () => {
-  beforeAll (async ()=>{
+  beforeAll(async () => {
     await connectDB();
   });
-  
-  describe('GET /api/users',()=>{
-    test('should respond with a 200 status code GET', async () => {
-      const res = await request.get('/api/users');
-       
-      expect(res.statusCode).toEqual(200);    
-    });
-  
-    test('should respond with an array of users GET', async () => {
-      const res = await request.get('/api/users');
-     
-      expect(res.body).toBeInstanceOf(Array);  
-    });  
-    afterAll(async ()=>{
-      await user.deleteMany();
-    });
-  });
-  
-  describe('GET /api/users/:id',()=>{
-    test('should respond with a 200 status code if search for id GET/:id', async () => {   
-      
-      const id='62461f487046652c36657e8f';
-
-      const res = await request.get(`/api/users/${id}`);
-      
-      expect(res.statusCode).toEqual(200);  
-    });
+  afterAll(async () => {
+    await users.deleteMany();
     
-    test('should respond with an array of user and with date specifics GET/:id', async () => {
-
-      const id='62461f487046652c36657e8f';
-      const res = await request.get(`/api/users/${id}`);
-     
-      expect(res.body).toEqual(expect.objectContaining({
-          email:expect.any(String),
-          firstName:expect.any(String),
-          lastName:expect.any(String),
-          password:expect.any(String),
-          role:expect.any(String),
-        }));
-    });
-
-    test('should respond with a 404 status code if search for id GET/:id', async () => {   
-      
-      const id='62461f4e8fpoiu';
-
-      const res = await request.get(`/api/users/${id}`);
-      
-      expect(res.statusCode).toEqual(404);
-      expect(res.body).toEqual({message:'error'});
-    });
-
+    await mongoose.connection.close();
   });
-  
   describe('POST /api/users',()=>{
     test('should respond with a 201 status code POST', async () => {
       const res = await request.post('/api/users').send({
         firstName: 'javier',
         lastName: 'rodriguez',
-        email: 'javier@rodriguezpo.com',
+        email: 'javiermsvsdark@rodriguezpo.com',
         password: '123',
       });
-
       expect(res.statusCode).toEqual(201);
       expect(res.body).toEqual(expect.objectContaining({
         email:expect.any(String),
@@ -80,7 +32,7 @@ describe('users EndPoints', () => {
         password:expect.any(String),
         role:expect.any(String),
       }));
-    });
+      });
 
     test('should respond with a 404 status code POST', async () => {
       const res = await request.post('/api/users').send({
@@ -90,56 +42,106 @@ describe('users EndPoints', () => {
         password: '',
       });
       expect(res.statusCode).toEqual(404);
-      expect(res.body).toEqual({message:'error'});
-      
-});
-
-  });
-
-  describe('PATCH /tasks/:id ', () => {
-    test('should respond with a 200 status code PACTH', async () => {
-      const id ='624277066d633186da8a6721' ;
-      const user = {
-        firstName: 'vicente',
-      };
+      expect(res.body).toEqual({message:'error'});  
+      });
+    });
+   
+  describe('GET /api/users',()=>{
+    test('should respond with a 200 status code GET', async () => {
+        const res = await request.get('/api/users');
+        
+        expect(res.statusCode).toEqual(200);    
+      });
     
-      const res = await request.patch(`/api/users/${id}`).send(user);
-      // Assert
-      expect(res.statusCode).toEqual(201);
+    test('should respond with an array of users GET', async () => {
+        const res = await request.get('/api/users');
+
+        expect(res.body).toBeInstanceOf(Array);  
+      }); 
+    }); 
+      
+  describe('GET /api/users/:id',()=>{
+    test('should respond with a 200 status code if search for id GET/:id', async () => { 
+      
+      const res = await request.get('/api/users');
+      const searchID=res.body[0]._id;
+      const respond = await request.get(`/api/users/${searchID}`);
+      
+      expect(respond.statusCode).toEqual(200);  
+      });
+
+    test('should respond with an array of user and with date specifics GET/:id', async () => {
+
+      const res = await request.get('/api/users');
+      const searchID=res.body[0]._id;
+      const respond = await request.get(`/api/users/${searchID}`);
+     
+      expect(respond.body).toEqual(expect.objectContaining({
+          email:expect.any(String),
+          firstName:expect.any(String),
+          lastName:expect.any(String),
+          password:expect.any(String),
+          role:expect.any(String),
+        }));
+      });
+      
+    test('should respond with a 404 status code if search for id GET/:id', async () => {   
+        
+        const id='62461f4e8fpoiu';
+        
+        const res = await request.get(`/api/users/${id}`);
+        
+        expect(res.statusCode).toEqual(404);
+        expect(res.body).toEqual({message:'error'});
+      });   
+
     });
     
-    test('should respond with the updated user PACTH', async () => {
-      
-      const id ='624277066d633186da8a6721' ;
+  describe('PATCH /tasks/:id ', () => {
+    test('should respond with a 200 status code PACTH', async () => {
       const user = {
         firstName: 'vicente',
       };
+      const res = await request.get('/api/users');
+      const searchID=res.body[0]._id;
+      const respond = await request.patch(`/api/users/${searchID}`).send(user);
+  
+      expect(respond.statusCode).toEqual(201);
+      });
+    
+    test('should respond with a object updated the user PACTH', async () => {
       
-      const res = await request.patch(`/api/tasks/${id}`).send(user);
+      const user = {
+        firstName: 'vicente',
+      };
+      const res = await request.get('/api/users');
+      const searchID=res.body[0]._id;
+      const respond = await request.patch(`/api/users/${searchID}`).send(user);
       
-      expect(res.body).toBeInstanceOf(Object); 
+      expect(respond.body).toBeInstanceOf(Object); 
       });
   
     test('should respond with a 404 status code if search for id PACTH',async ()=>{
       const id='62461f4e8fpoiu';
       const user = {
-        firstName: 'vicente',
+          firstName: 'vicente',
       };
       const res = await request.patch(`/api/users/${id}`).send(user);
       
       expect(res.statusCode).toEqual(500);
       expect(res.body).toEqual({message:'error'});
+      });
     });
-
-  });
-
+  
   describe('DELETE /tasks/:id', () => {
     test('should respond with a 200 status code when delete user for body DELETE/:id', async () => {  
-      const res = await request.delete(`/api/users`).send({
-        email: 'javier@rodriguezpo.com',
-      });
       
-      expect(res.statusCode).toEqual(200);
+      const res = await request.get('/api/users');
+      const searchID=res.body[0]._id;
+      const response = await request.delete(`/api/users/${searchID}`)
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.body).toBeInstanceOf(Object); 
     });
 
     test('should respond with a 404 status code if search for id DELETE/:id', async () => {   
@@ -151,9 +153,10 @@ describe('users EndPoints', () => {
       expect(res.statusCode).toEqual(404);
       expect(res.body).toEqual({message:'error'});
     });
-         
   });
-
-});    
+  
+});
+   
+   
 
 
