238cab8cf6831302037a54883e0be185f7b65189
wendy-medina <endyleyms@gmail.com>
2022-04-08 18:38:29 -0500

    tests de clinic History

--- api/clinicHistory/clinicHistory.model.js
+++ api/clinicHistory/clinicHistory.model.js
@@ -2,11 +2,11 @@ const mongoose = require ('mongoose');
 const clinicHistory = new mongoose.Schema({
     //idpatient:{type: Number, required: true},
     //iddoctor:{type: Number, required: true},
-    description: {type: String, required: true},
+    description: {type: String, required: true, unique:true},
     userId:{
       type:mongoose.Schema.Types.ObjectId,
       ref:'User',
-      required: true,
+      required: false,
   },
 },
   
--- api/clinicHistory/testClinicH/clinicHistory.test.js
+++ api/clinicHistory/testClinicH/clinicHistory.test.js
@@ -1,18 +1,25 @@
 const supertest = require('supertest');
 const app = require('../../../app');
+const mongoose = require ('mongoose')
 const connectDB = require('../../../config/database');
 const clinicHistory = require('../clinicHistory.model');
 const request = supertest(app);
 
 
 describe('clinic History Endpoints', () => {
-    
-  
+  beforeAll (async ()=>{
+    await connectDB();
+  });
+  afterAll (async ()=>{
+    await mongoose.connection.close();
+  });
+
+  describe ('Post clinicHistory', ()=>{
     test('Post clinicHistory', async () => {
       const res = await request
         .post('/api/clinicHistorys')
         .send({
-          description: 'paciente con depresión',
+          description: 'paciente con depresión'          
         });
   
       expect(res.statusCode).toEqual(201);
@@ -20,15 +27,37 @@ describe('clinic History Endpoints', () => {
         description: 'paciente con depresión',
       }));
     });
-  
-    test('should not create a same clinicHistory', async () => {
-      const res = await request
-        .post('/api/clinicHistorys')
-        .send({
-          description: 'paciente con depresión',
-        });
-  
-      expect(res.statusCode).toEqual(404);
-      expect(res.body).toEqual({message: 'error'});
+
+  })
+
+  describe('GetAll cliniciHistory',  ()=>{
+    test('should respond with a 200 status code GET', async () => {
+      const res = await request.get('/api/clinicHistorys');
+       
+      expect(res.statusCode).toEqual(200);    
     });
-  });
+  
+    test('should respond with an array of users GET', async () => {
+      const res = await request.get('/api/clinicHistorys');
+     
+      expect(res.body).toBeInstanceOf(Array);  
+    });  
+    
+  })
+
+  describe('Get clinicHistory', ()=>{
+    test('should respond with a 200 status code if search for id GET/:id', async () => {   
+      
+      const id='6250afbbd532399353b938e7';
+
+      const res = await request.get(`/api/clinicHistorys/${id}`);
+      
+      expect(res.statusCode).toEqual(200);  
+    });
+  })
+
+  
+  
+    
+  
+ });
