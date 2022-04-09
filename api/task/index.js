5472659559ec30bfb461ba384df58249e8a48adb
Diego Cubides <95053070+d1360cub@users.noreply.github.com>
2022-04-09 00:08:47 -0500

    adds eslint (#17)
    Co-authored-by: ojosdegatonegro <diegofernandocubides@gmail.com>

--- api/task/index.js
+++ api/task/index.js
@@ -1,12 +1,15 @@
-const {Router} = require ('express');
-const {isAuthenticated, hasRole}=require('../../auth/auth.service');
-const {handlerAllTasks, handlerOneTask, handlerCreateTask, handlerUpdateTask, handlerDeleteTask} = require ('./task.controller');
+const { Router } = require('express');
+const { isAuthenticated, hasRole } = require('../../auth/auth.service');
+const {
+  handlerAllTasks, handlerOneTask, handlerCreateTask, handlerUpdateTask, handlerDeleteTask,
+} = require('./task.controller');
+
 const router = Router();
 
-router.get('/', handlerAllTasks)
-router.get('/:id', handlerOneTask)
-router.post('/', isAuthenticated(), handlerCreateTask)
-router.patch('/:id', hasRole(['doctor']), handlerUpdateTask)
-router.delete('/:id', hasRole(['doctor']), handlerDeleteTask)
+router.get('/', handlerAllTasks);
+router.get('/:id', handlerOneTask);
+router.post('/', isAuthenticated(), handlerCreateTask);
+router.patch('/:id', hasRole(['doctor']), handlerUpdateTask);
+router.delete('/:id', hasRole(['doctor']), handlerDeleteTask);
 
-module.exports = router
+module.exports = router;
--- api/task/task.service.js
+++ api/task/task.service.js
@@ -1,28 +1,30 @@
-const taskModel = require ('./task.model');
+const taskModel = require('./task.model');
 
-async function getAllTasks (){
-    const tasks = await taskModel.find();
-    return (tasks)
+async function getAllTasks() {
+  const tasks = await taskModel.find();
+  return (tasks);
 }
 
-async function getOneTask (id){
-    const task = await taskModel.findById(id).populate({path:'userId', select: 'firstName lastName'})
-    return (task)
+async function getOneTask(id) {
+  const task = await taskModel.findById(id).populate({ path: 'userId', select: 'firstName lastName' });
+  return (task);
 }
 
-async function createTask (task){
-    const newTask = await taskModel.create(task);
-    return (newTask)
+async function createTask(task) {
+  const newTask = await taskModel.create(task);
+  return (newTask);
 }
 
-async function updatedTask(id, task){
-    const taskUpdated = await taskModel.findByIdAndUpdate(id, task, {new: true})
-    return(taskUpdated)
+async function updatedTask(id, task) {
+  const taskUpdated = await taskModel.findByIdAndUpdate(id, task, { new: true });
+  return (taskUpdated);
 }
 
-async function deleteTask (id){
-    const taskDeleted = await taskModel.findByIdAndDelete(id)
-    return(taskDeleted)
+async function deleteTask(id) {
+  const taskDeleted = await taskModel.findByIdAndDelete(id);
+  return (taskDeleted);
 }
- 
-module.exports = {getAllTasks, getOneTask, createTask, updatedTask, deleteTask}
+
+module.exports = {
+  getAllTasks, getOneTask, createTask, updatedTask, deleteTask,
+};
