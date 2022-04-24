2ef28cf17cf02868d5d3a5b4530623d18d5e09c8
Wendy Medina <78057584+endyleyms@users.noreply.github.com>
2022-04-24 14:43:57 -0500

    Feature/wendy back (#31)
    * modificacion servicio historia clinica para organizar la data en front de historia clinica por fecha
    
    * modificacion controller user agregando error a la respuesta
    
    * modificacion
    
    * Update user.controller.js
    
    Co-authored-by: Diego Cubides <95053070+d1360cub@users.noreply.github.com>

--- auth/local/local.controller.js
+++ auth/local/local.controller.js
@@ -6,16 +6,16 @@ async function handlerLogin(request, response) {
   try {
     const user = await getUserByEmail(email);
     if (!user) {
-      return response.status(401).json({ message: 'Invalid email or password' });
+      return response.status(401).json({ error: { message: 'Email o contraseña invalido' } });
     }
     const isMatch = await user.comparePassword(password);
     if (!isMatch) {
-      return response.status(401).json({ message: 'Invalid email or password' });
+      return response.status(401).json({ error: { message: 'Email o contraseña invalido' } });
     }
     const token = signToken(user.profile);
     return response.status(200).json({ user: user.profile, token });
   } catch (error) {
-    return response.status(400).json(error);
+    return response.status(400).json({ error });
   }
 }
 
