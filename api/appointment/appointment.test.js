6fa9b7bb946adf79da8e289faeb98128a71efe36
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-04-28 16:50:27 -0500

    console.log appointment.test

--- api/appointment/appointment.test.js
+++ api/appointment/appointment.test.js
@@ -40,6 +40,7 @@ describe('Appointments EndPoints', () => {
     test('should respond with a 404 when task is not found', async () => {
       const id = 'dfsafsadfsdfsd';
       const getWrongTask = await request.get(`/api/appointments/${id}`);
+      console.log(getWrongTask);
       expect(getWrongTask.statusCode).toEqual(404);
     });
   });
