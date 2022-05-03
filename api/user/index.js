9acce3786dc3f24f87464424134b991a51ff2e69
wendy-medina <endyleyms@gmail.com>
2022-05-03 08:40:16 -0500

    code comments eliminated

--- api/user/index.js
+++ api/user/index.js
@@ -14,6 +14,5 @@ router.get('/:id', handlerGetUserById);
 router.post('/', handlerCreateUser);
 router.patch('/:id', handlerUpdateUser);
 router.delete('/:id', handlerDeleteUser);
-// router.delete('/', handlerDeleteUser);
 
 module.exports = router;
