74a9f394c9f71a834293fd64f83ce66fd3cf4b89
wendy-medina <endyleyms@gmail.com>
2022-05-02 21:08:58 -0500

    validation user and fixed conflicts

--- api/user/user.factory.js
+++ api/user/user.factory.js
@@ -9,16 +9,10 @@ const patientUser = {
 const doctorUser = {
   firstName: 'prueba',
   lastName: 'test',
-<<<<<<< HEAD
-  email: 'prueba@test.com',
-  password: '123',
-  phone: '3214567895',
-=======
   email: 'doctortest@test.com',
   password: '123',
   phone: '3214567895',
   role: 'doctor',
->>>>>>> 5b0dcc1c801ce2997210b9a15bb9c472c6247c0f
 };
 
 module.exports = {
